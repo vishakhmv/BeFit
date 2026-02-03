33980722edaa605e92ae479cc71957480f6336f7
Johan Paul <johanjohn405@gmail.com>
2026-02-03 23:11:12 +0530

    Initial version of landing page

---
+++ blood-analyzer/src/App.js
@@ -0,0 +1,39 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <nav className="navbar">
+        <h1 className="logo">BeFit</h1>
+        <div className="nav-links">
+          <a href="#">About us</a>
+          <a href="#">Help & Support</a>
+        </div>
+      </nav>
+      <hr></hr>
+
+      <section className="hero">
+        <div className="hero-text">
+          <h2>
+            Master Your Health <br /> with Smart Data.
+          </h2>
+          <p>
+            Turn your blood reports into actionable insights and better
+            habits for a healthier life.
+          </p>
+
+          <div className="buttons">
+            <button className="signup">Sign up</button>
+            <button className="login">Log in</button>
+          </div>
+        </div>
+
+        <div className="hero-image">
+          <img src="/heroimage.jpg" alt="Health" />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
