0e39daffb94cac6cf6192494fd9ce83f83a7456c
Vishakh M V <vishakhmv667@gmail.com>
2026-02-12 11:44:44 +0530

    Make database connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
 let port = 5000;
 
 let app = express();
 app.use(express.urlencoded({ extended: true }));
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "befit",
+  password: "",
+  port: 5432,
+});
+db.connect();
 
 app.listen(port, () => {
   console.log(`Your app is listening to port ${port}`);
