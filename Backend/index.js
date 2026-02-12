9c389360e9b1b50c2c969238e7db1200c76b7966
Vishakh M V <vishakhmv667@gmail.com>
2026-02-12 15:29:42 +0530

    Add signup route with database insertion

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser";
 import pg from "pg";
 
 let port = 5000;
@@ -15,6 +14,23 @@ const db = new pg.Client({
   port: 5432,
 });
 db.connect();
+
+app.post("/signup", async (req, res) => {
+  try {
+    let name = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password;
+    let dob = req.body.dob;
+    await db.query(
+      "INSERT INTO users (name, email, password, dob) VALUES ($1, $2, $3, $4)",
+      [name, email, password, dob],
+    );
+    res.redirect("/login");
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Your app is listening to port ${port}`);
