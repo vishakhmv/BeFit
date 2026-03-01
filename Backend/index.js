997de3b35a7f1281f321bcfcc194cc890f67fe07
Vishakh M V <vishakhmv667@gmail.com>
2026-03-01 14:48:34 +0530

    modified signup and login with bcrypt

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import pg from "pg";
+import bcrypt from "bcrypt";
 
 let port = 5000;
+const saltRounds = 10;
 
 let app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -21,17 +23,23 @@ app.post("/signup", async (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
     let dob = req.body.dob;
-    let result=await db.query("select * from users where email=$1",[email]);
-    if(result.rows.length===0){
-    await db.query(
-      "INSERT INTO users (name, email, password, dob) VALUES ($1, $2, $3, $4)",
-      [name, email, password, dob],
-    );
-    res.render("login");
-  }
-  else{
-    res.render("signup",{error:"Email already exist"});
-  }
+    let result = await db.query("select * from users where email=$1", [email]);
+    if (result.rows.length === 0) {
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send("Signup failed");
+        } else {
+          await db.query(
+            "INSERT INTO users (name, email, password, dob) VALUES ($1, $2, $3, $4)",
+            [name, email, hash, dob],
+          );
+          res.render("login");
+        }
+      });
+    } else {
+      res.render("signup", { error: "Email already exist" });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send("Signup failed");
@@ -47,11 +55,18 @@ app.post("/login", async (req, res) => {
     ]);
     if (result.rows.length > 0) {
       let user_password = result.rows[0].password;
-      if (password === user_password) {
-        res.render("home");
-      } else {
-        res.render("login", { error: "Email or password is incorrect" });
-      }
+      bcrypt.compare(password, user_password, (err, valid) => {
+        if (err) {
+          console.error(err);
+          res.render("login", { error: "An error occurred during login" });
+        } else {
+          if (valid) {
+            res.render("home");
+          } else {
+            res.render("login", { error: "Email or password is incorrect" });
+          }
+        }
+      });
     } else {
       res.render("login", { error: "Email or password is incorrect" });
     }
