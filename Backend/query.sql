e38e95058cf0ff7a22396dc75ec2850edb5d8da9
Vishakh M V <vishakhmv667@gmail.com>
2026-03-06 09:18:41 +0530

    modified query.sql

--- Backend/query.sql
+++ Backend/query.sql
@@ -3,5 +3,32 @@ CREATE TABLE users (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
     password TEXT NOT NULL,
-    dob DATE,
+    dob DATE
+);
+
+CREATE TABLE diet (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    day VARCHAR(20),
+    meal_time VARCHAR(10),
+    food TEXT
+);
+
+CREATE TABLE exercise (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    day VARCHAR(20),
+    exercise TEXT
+);
+
+CREATE TABLE sleep (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    sleep_hour VARCHAR(20)
+);
+
+CREATE TABLE water (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    water VARCHAR(20)
 );
